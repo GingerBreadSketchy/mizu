96ef2336f3223b55c95d43d1d537c382dafe7218
GingerBreadSketchy <58087375+GingerBreadSketchy@users.noreply.github.com>
2022-07-21 16:57:27 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,18 +4,18 @@ const chalk = require("chalk");
 // Other
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "917003213983";
+  gg = "254792893310";
 }
 
 global.owner = gg.split(",");
 global.mongodb = process.env.MONGODB || "none";
 global.pgdb = process.env.DATABASE_URL || "none";
 global.port = process.env.PORT || 8080;
-(global.packname = process.env.PACKNAME || "kaguya"),
+(global.packname = process.env.PACKNAME || "GingerSketchy"),
   (global.prefix = process.env.PREFIX || "-"),
-  (global.author = process.env.AUTHOR || "ARUS"),
-  (global.sessionName = "arus"),
-  (global.name = process.env.NAME || "Mizuhara"),
+  (global.author = process.env.AUTHOR || "GINGER"),
+  (global.sessionName = "session"),
+  (global.name = process.env.NAME || "NYARU"),
   (global.thumb = fs.readFileSync("./trash/info.jpeg"));
 
 let file = require.resolve(__filename);
